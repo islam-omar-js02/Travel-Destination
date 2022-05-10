c559402a23e27be54de10bac6cc7111f852303d1
Omar Sawalmeh <sawalmehomar10@gmail.com>
2022-05-10 20:28:09 +0300

    Upload First Version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+
+function Footer(props){
+    return(
+        <>
+            <a href="https://mail.google.com/mail/u/0/#inbox" >Omar Sawalmeh</a>
+            <br></br>
+            <a href="islamrwashdeh@yahoo.com" >Islam alrwashdeh</a>
+        </>
+    );
+}
+export default Footer;
